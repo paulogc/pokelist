5ddb6885f83c43355964e13a2f983d19b3869dfd
Guedes <paulo.guedes@vistajet.com>
2017-12-26 22:24:22 -0200

    transaction to es6

--- app/adapters/pokemon.js
+++ app/adapters/pokemon.js
@@ -3,7 +3,5 @@ import DS from 'ember-data';
 export default DS.RESTAdapter.extend({
   host: 'https://pokeapi.co',
   namespace: '/api/v2',
-  pathForType: function(type) {
-    return type;
-  },
+  pathForType: type => type
 });
