6184a508f9b9927981baac6291d342e2176cb95e
Guedes <paulo.guedes@vistajet.com>
2017-12-27 23:28:09 -0200

    load more button

--- app/routes/pokemon/index.js
+++ app/routes/pokemon/index.js
@@ -3,22 +3,34 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   isLoading: true,
+  isDisabled: false,
+
   model() {
     return this.store.query(
       'pokemon',
       {
-        limit: 100,
-        offset: 20
+        limit: 20, 
+        offset: 20,
       }
     ).then(pokemon => {
       this.set('isLoading', false);
+      this.set('limit', params.limit || 20);
+      this.set('offset', params.offset || 20);
       return pokemon;
     });
   },
 
   actions: {
-    editPokemon() {
-
+    loadMore() {
+      this.transitionTo(
+        'index',
+        {
+          queryParams: {
+            limit: this.get('limit') + 20,
+            offset: this.get('offset') + 20,
+          }
+        }
+      );
     }
   }
 });
