4a10d8ff2d0143f67102c2beb33fad35e6061fc6
Guedes <paulo.guedes@vistajet.com>
2017-12-27 22:07:31 -0200

    change PUT headers

--- app/adapters/pokemon.js
+++ app/adapters/pokemon.js
@@ -3,5 +3,15 @@ import DS from 'ember-data';
 export default DS.RESTAdapter.extend({
   host: 'https://pokeapi.co',
   namespace: '/api/v2',
-  pathForType: type => type
+  pathForType: type => type,
+
+  ajax: function(url, type, hash) {
+    if (hash && type === 'PUT') {
+      hash.headers = {
+        'put': 'put-pokemon',
+      }
+    }
+
+    return this._super(url, type, hash);
+  }
 });
