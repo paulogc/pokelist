1945e30b80532458855db89a0726a55c3cbf5002
Guedes <paulo.guedes@vistajet.com>
2017-12-29 07:45:42 -0200

    put on editing pokemon

--- app/controllers/pokemon/edit.js
+++ app/controllers/pokemon/edit.js
@@ -1,4 +1,5 @@
 import Controller from '@ember/controller';
+import moment from 'moment';
 
 export default Controller.extend({
   isSaved: false,
@@ -20,6 +21,13 @@ export default Controller.extend({
     savePokemon() {
       this.set('isSaved', true);
       this.set('willSave', false);
+
+      this.store.findRecord('pokemon', this.get('model.id')).then((pokemon) => {
+        pokemon.set('name', this.get('model.name'));
+        pokemon.set('abilities', this.get('model.abilities'));
+        pokemon.set('updateDate', moment().format('YYYY-MM-DD-HH-mm'));
+        pokemon.save();
+      })
     },
 
     didTransition() {
